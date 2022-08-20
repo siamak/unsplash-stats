02f23d84a906c6e7c602e06442e847daf404adcd
Siamak Mokhtari <s.mokhtari75@gmail.com>
2022-08-20 22:36:05 +0430

    Version 2.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react";
-import Head from "next/head";
 import dynamic from "next/dynamic";
 import NProgress from "nprogress";
 import useSWRInfinite from "swr/infinite";
@@ -13,6 +11,10 @@ const PAGE_SIZE = 12;
 
 const Photo = dynamic(() => import("../src/components/photo.card"), {
 	ssr: false,
+});
+
+const Layout = dynamic(() => import("../src/components/root.layout"), {
+	ssr: true,
 });
 
 type Props = { user: string };
@@ -36,7 +38,7 @@ export default function Unsplash({ user }: Props) {
 	const isEmpty = data?.[0]?.length === 0;
 	const isReachingEnd =
 		isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE);
-	const isRefreshing = isValidating && data && data.length === size;
+	// const isRefreshing = isValidating && data && data.length === size;
 
 	useEffect(() => {
 		setNextPage(Boolean(!isReachingEnd));
@@ -54,7 +56,7 @@ export default function Unsplash({ user }: Props) {
 		setSize((prev) => prev + 1);
 	};
 
-	const [albumRef] = useInfiniteScroll({
+	const [ref] = useInfiniteScroll({
 		loading: isValidating,
 		disabled: !!error,
 		onLoadMore,
@@ -63,81 +65,71 @@ export default function Unsplash({ user }: Props) {
 	});
 
 	return (
-		<>
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>@{user} – Unsplash statistics</title>
-			</Head>
+		<Layout user={user}>
+			<div className="form">
+				<input
+					value={val}
+					className="form-input"
+					onChange={(e) => setVal(e.target.value)}
+					placeholder="onlysiamak"
+				/>
+				<button
+					className="form-button"
+					onClick={() => {
+						setRepo(val);
+						setSize(1);
+					}}
+				>
+					Show photos
+				</button>
+			</div>
 
-			<div className="wrapper">
-				<div className="form">
-					<input
-						value={val}
-						className="form-input"
-						onChange={(e) => setVal(e.target.value)}
-						placeholder="onlysiamak"
-					/>
-					<button
-						className="form-button"
-						onClick={() => {
-							setRepo(val);
-							setSize(1);
-						}}
-					>
-						Show photos
-					</button>
-				</div>
+			<div className="grid">
+				{photos.map((photo: Item, i) => (
+					<Photo i={i} item={photo} key={photo.id} />
+				))}
+			</div>
 
-				<div className="grid">
-					{photos.map((photo: Item, i) => (
-						<Photo i={i} item={photo} key={photo.id} />
-					))}
-				</div>
-
-				<div className="pagination" ref={albumRef}>
-					{isValidating && <div>Loading...</div>}
-					<p>
-						showing {size} page(s) of {isLoadingMore ? "..." : photos.length}{" "}
-						photo(s){" "}
-						<button
-							disabled={isLoadingMore || isReachingEnd}
-							onClick={() => setSize(size + 1)}
-						>
-							{isLoadingMore
-								? "loading..."
-								: isReachingEnd
-								? "no more photos"
-								: "load more"}
-						</button>
-						{/* <button disabled={isRefreshing} onClick={() => mutate()}>
+			<div className="pagination" ref={ref}>
+				<p>
+					showing {size} page(s) of {isLoadingMore ? "..." : photos.length}{" "}
+					photo(s)
+					{/* <button disabled={isRefreshing} onClick={() => mutate()}>
 							{isRefreshing ? "refreshing..." : "refresh"}
 						</button>
 						<button disabled={!size} onClick={() => setSize(0)}>
 							clear
 						</button> */}
-					</p>
-					{isEmpty ? <p>Yay, no photos found.</p> : null}
-				</div>
+				</p>
+				<button
+					disabled={isLoadingMore || isReachingEnd}
+					onClick={() => setSize(size + 1)}
+					className="btn"
+				>
+					{isLoadingMore
+						? "Loading..."
+						: isReachingEnd
+						? "No More Photos"
+						: "Load More"}
+				</button>
 
-				<div className="copyright">
-					<span className="hr" />
-					<div>
-						Created by <a href="https://siamak.me">Siamak</a>.
-					</div>
-				</div>
+				{isEmpty ? <p>Yay, no photos found.</p> : null}
 			</div>
 			<style jsx>{`
-				.wrapper {
-					width: 100%;
-
-					padding: 4rem;
-					margin-right: auto;
-					margin-left: auto;
-				}
-				@media (max-width: 768px) {
-					.wrapper {
-						padding: 2rem 1rem;
-					}
+				.btn {
+					background-color: #141414;
+					border: 1px solid rgba(54, 54, 54, 0.6);
+					opacity: 1;
+					font-weight: 500;
+					position: relative;
+					outline: none;
+					border-radius: 50px;
+					padding: 1rem;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					cursor: pointer;
+					margin-top: 0.5rem;
 				}
 
 				.form {
@@ -185,37 +177,8 @@ export default function Unsplash({ user }: Props) {
 					flex-direction: column;
 					align-items: center;
 				}
-
-				.copyright {
-					display: flex;
-					justify-content: center;
-					margin: 2rem 0;
-					flex-direction: column;
-					align-items: center;
-				}
-
-				.copyright a {
-					color: #9677ff;
-				}
-
-				.hr {
-					display: inline-flex;
-					width: 4rem;
-					height: 2px;
-					margin-bottom: 1rem;
-					background: rgba(255, 255, 255, 0.1);
-				}
-
-				@media (prefers-color-scheme: light) {
-					.hr {
-						background: rgba(0, 0, 0, 0.1);
-					}
-					.copyright a {
-						color: #4114db;
-					}
-				}
 			`}</style>
-		</>
+		</Layout>
 	);
 }
 
